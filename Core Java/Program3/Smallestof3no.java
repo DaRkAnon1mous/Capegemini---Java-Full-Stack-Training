45c520cdabcb5b8ae15fe320ff5a60100cd21708
DarkAnon1mous <shreydikshant144@gmail.com>
2025-02-19 02:24:55 +0530

    Commit

---
+++ Core Java/Program3/Smallestof3no.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+public class Smallestof3no {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter numbers ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if(a == b && b == c) {
+            System.out.print("All numbers are equal");
+        } 
+        else if(a <= b && a <= c) {
+            System.out.print("Smallest number is: " + a);
+        } 
+        else if(b <= a && b <= c) {
+            System.out.print("Smallest number is: " + b);
+        } 
+        else {
+            System.out.print("Smallest number is: " + c);
+        }
+}
+        
+    
+}
+    
+
