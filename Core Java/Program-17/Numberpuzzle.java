b5f40a275c10dbf3206d4a2338c052ce9bcb2cf3
DarkAnon1mous <shreydikshant144@gmail.com>
2025-02-21 02:11:43 +0530

    Chnges

--- Core Java/Program-17/Numberpuzzle.java
+++ Core Java/Program-17/Numberpuzzle.java
@@ -1,3 +1,60 @@
+/*
+ * Desciption
+ * Kevin is trying to solve a number puzzle. If the number is greater than fifty, he wants the difference between the digits of the number; otherwise, if the number is less than or equal to fifty, he wants to reverse the number and find the difference in digits. Help Kevin to solve the problem using a Java program.
+
+Requirements:
+
+The input number should be a two digit integer, else display "Invalid number".
+
+Note:
+
+In the Sample Input / Output provided, the highlighted text in bold corresponds to the input given by the user, and the rest of the text represents the output.
+Ensure to follow the object-oriented specifications provided in the question description.
+Ensure to provide the names for classes, attributes, and methods as specified in the question description.
+Adhere to the code template, if provided.
+Please do not use System.exit(0) to terminate the program.
+
+Sample Input/Output 1:
+
+Enter the number
+
+56
+
+-1
+
+Explanation:
+
+56 is greater than 50, so subtract 5 - 6 = -1
+
+
+
+Sample Input/Output 2:
+
+Enter the number
+
+48
+
+4
+
+Explanation:
+
+48 is less than 50, so reverse 48 to 84 then subtract 8 - 4 = 4
+
+
+
+Sample Input/Output 3:
+
+Enter the number
+
+9
+
+Invalid number
+ * 
+ * 
+ */
+
+
+
 import java.util.Scanner;
 
 
