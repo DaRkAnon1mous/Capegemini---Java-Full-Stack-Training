5cf1ae66275397fe0c0849670b4e44c46d196da3
DarkAnon1mous <shreydikshant144@gmail.com>
2025-03-04 14:33:33 +0530

    Commit Program

---
+++ Core Java/Campus_Radio_Frequency(excep_hand)/StationNotAvailableException.java
@@ -0,0 +1,7 @@
+//make the necessary change to make this class a Exception
+public class StationNotAvailableException extends Exception{
+	public StationNotAvailableException(String m) {
+		super(m);
+	}
+	// fill the code
+}
